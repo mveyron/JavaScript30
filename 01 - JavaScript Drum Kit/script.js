172392a018c93173804da15f3b5e7da5e3bc0026
mveyron <aspacehonk@gmail.com>
2017-07-29 20:01:37 +0300

    wow I can use consts

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,28 +1,29 @@
-var keyDownHandler = function (event) {
-	var audioForKey = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-	var buttonForKey = document.querySelector(`button[data-key="${event.keyCode}"]`);
+const keyDownHandler = function (event) {
+	const audioForKey = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+	const buttonForKey = document.querySelector(`button[data-key="${event.keyCode}"]`);
 
 	if (audioForKey && buttonForKey) {
 		buttonForKey.classList.add('playing');
 		audioForKey.addEventListener('ended', function () {
 			buttonForKey.classList.remove('playing');
 		});
+		audioForKey.currentTime = 0;
 		audioForKey.play();
 	}
 };
 
 document.addEventListener("DOMContentLoaded", function () {
 	window.addEventListener('keydown', keyDownHandler);
-	var buttons = document.querySelectorAll('button[data-key]');
-	var audios = document.querySelectorAll('audio[data-key]');
-	var soundNames = {};
+	const buttons = document.querySelectorAll('button[data-key]');
+	const audios = document.querySelectorAll('audio[data-key]');
+	const soundNames = {};
 	audios.forEach(function(a) {
-		var audioCode = a.dataset.key;
-		var audioName = a.src.match(/sounds\/([a-zA-Z]+)\.wav$/i);
+		const audioCode = a.dataset.key;
+		const audioName = a.src.match(/sounds\/([a-zA-Z]+)\.wav$/i);
 		soundNames[audioCode] = audioName[1];
 	});
 	buttons.forEach(function(b) {
-		var soundElement = document.createElement('span');
+		const soundElement = document.createElement('span');
 		soundElement.classList.add('sound');
 		soundElement.innerHTML = soundNames[b.dataset.key];
 		b.appendChild(soundElement);
