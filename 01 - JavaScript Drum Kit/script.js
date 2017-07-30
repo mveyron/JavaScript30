a80ef5ba8df7e719a4280052ff43fc289ddba27e
mveyron <aspacehonk@gmail.com>
2017-07-30 09:25:05 +0300

    01: do not assign listener on each button press

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,25 +1,31 @@
 const keyDownHandler = function (event) {
 	const audioForKey = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-	const buttonForKey = document.querySelector(`button[data-key="${event.keyCode}"]`);
+	const buttonForKey = document.querySelector(`button[data-key="${event.keyCode}"]`);	
 
 	if (audioForKey && buttonForKey) {
 		buttonForKey.classList.add('playing');
-		audioForKey.addEventListener('ended', function () {
-			buttonForKey.classList.remove('playing');
-		});
 		audioForKey.currentTime = 0;
 		audioForKey.play();
 	}
 };
 
-document.addEventListener("DOMContentLoaded", function () {
-	window.addEventListener('keydown', keyDownHandler);
+window.addEventListener('keydown', keyDownHandler);
 
-	document.querySelectorAll('button[data-key]').forEach(function(b) {
-		const soundElement = document.createElement('span');
-		soundElement.classList.add('sound');
-		const audioElelement = document.querySelector(`audio[data-key="${b.dataset.key}"]`);
-		soundElement.innerHTML = audioElelement.src.match(/sounds\/([a-zA-Z]+)\.wav$/i)[1];
-		b.appendChild(soundElement);
+const audios = document.querySelectorAll(`audio[data-key]`);	
+audios.forEach(function (a) {
+	const audioKey = a.dataset.key;
+	const buttonForKey = document.querySelector(`button[data-key="${audioKey}"]`);
+	a.addEventListener('ended', function (e) {
+		buttonForKey.classList.remove('playing');
 	});
+});
+
+
+const buttons = document.querySelectorAll('button[data-key]');
+buttons.forEach(function(b) {
+	const soundElement = document.createElement('span');
+	soundElement.classList.add('sound');
+	const audioElement = document.querySelector(`audio[data-key="${b.dataset.key}"]`);
+	soundElement.innerHTML = audioElement.src.match(/sounds\/([a-zA-Z]+)\.wav$/i)[1];
+	b.appendChild(soundElement);
 });
