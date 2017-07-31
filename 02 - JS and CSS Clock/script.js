48ba337190077b85ba643b737fec9facc29d4a17
mveyron <aspacehonk@gmail.com>
2017-07-31 23:05:37 +0300

    02: add ticking and update time each sec to avoid min hand moving when sec hand is not on 12

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,21 +1,26 @@
 const ONE_SEC = 1000;
-const ONE_MIN = ONE_SEC * 60;
-const ONE_HOUR = ONE_MIN * 60;
+const transitonRule = 'transform 0.05s';
+const secondsHand = document.querySelector('.second-hand');
+const minutesHand = document.querySelector('.min-hand');
+const hoursHand = document.querySelector('.hour-hand');
+
+const setAngle = (hand, angle) => {
+	hand.style.transition = angle === 0 ? 'none' : transitonRule;
+	hand.style.transform = `rotate(${angle}deg)`;
+}
 
 const updateSeconds = () => {
 	const now = new Date();
 	const seconds = now.getSeconds();
 	const angle = seconds * 6;
-	const secondsHand = document.querySelector('.second-hand');
-	secondsHand.style.transform = `rotate(${angle}deg)`;
+	setAngle(secondsHand, angle);
 };
 
 const updateMinutes = () => {
 	const now = new Date();
 	const minutes = now.getMinutes();
 	const angle = minutes * 6;
-	const minutesHand = document.querySelector('.min-hand');
-	minutesHand.style.transform = `rotate(${angle}deg)`;
+	setAngle(minutesHand, angle);
 };
 
 const updateHours = () => {
@@ -23,14 +28,14 @@ const updateHours = () => {
 	const hours = now.getHours();
 	const hoursOnClock = hours > 12 ? hours - 12 : hours;
 	const angle = hoursOnClock * 30;
-	const hoursHand = document.querySelector('.hour-hand');
-	hoursHand.style.transform = `rotate(${angle}deg)`;
+	setAngle(hoursHand, angle);
 };
 
-updateSeconds();
-updateMinutes();
-updateHours();
+const updateTime = () => {
+	updateSeconds();
+	updateMinutes();
+	updateHours();
+};
 
-setInterval(updateSeconds, ONE_SEC);
-setInterval(updateMinutes, ONE_MIN);
-setInterval(updateHours, ONE_HOUR);
+updateTime();
+setInterval(updateTime, ONE_SEC);
