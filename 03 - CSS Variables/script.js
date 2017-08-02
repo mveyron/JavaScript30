570140c5fc338351a2cc767fafa825d9c153e4a3
mveyron <aspacehonk@gmail.com>
2017-08-03 00:02:27 +0300

    03: fill controls initial values from CSS and adjust to updated image size

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -2,6 +2,10 @@ const imageBorderColor = '--image-border-color';
 const imageBorderStyle = '--image-border-style';
 const imageBorderWidth = '--image-border-width';
 
+const borderStyleElement = document.getElementById('border-style');
+const borderColorElement = document.getElementById('border-color');
+const borderWidthElement = document.getElementById('border-width');
+
 const updateBorderColor = (e) => {
 	document.documentElement.style.setProperty(imageBorderColor, e.target.value);
 }
@@ -14,6 +18,16 @@ const updateBorderWidth = (e) => {
 	document.documentElement.style.setProperty(imageBorderWidth, `${e.target.value}px`);
 }
 
-document.getElementById('border-color').addEventListener('change', updateBorderColor);
-document.getElementById('border-style').addEventListener('change', updateBorderStyle);
-document.getElementById('border-width').addEventListener('change', updateBorderWidth);
+const initialize = () => {
+	const initialStyles = getComputedStyle(document.documentElement);
+
+	borderColorElement.value = initialStyles.getPropertyValue(imageBorderColor).trim();
+	borderStyleElement.value = initialStyles.getPropertyValue(imageBorderStyle).trim();
+	borderWidthElement.value = initialStyles.getPropertyValue(imageBorderWidth).trim().substr(0,1);
+};
+
+borderColorElement.addEventListener('change', updateBorderColor);
+borderStyleElement.addEventListener('change', updateBorderStyle);
+borderWidthElement.addEventListener('change', updateBorderWidth);
+
+initialize();
